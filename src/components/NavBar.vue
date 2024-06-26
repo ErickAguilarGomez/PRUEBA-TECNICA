db7e0fec5a82659af536dffd1238beb9c880b82f
ErickAguilarGomez <lemayor01@hotmail.com>
2024-06-25 22:40:17 -0500

    "creando axios"

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="container  h-screen w-6">
+        <h1 class="text-6xl">hola</h1>
+        <h1 class="text-6xl">aqui se viene</h1>
+        <h2 class="text-6xl"> about</h2>
+    </nav>
+</template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="text-center flex  justify-center items-center flex-col">
+      <h1 class="text-4xl font-bold tracking-widest text-sky-700">Countries APP</h1>
+      <h6 class="mt-4 text-xl">Una App de búsqueda de países</h6>
+  
+    </div>
+  </template>
+  
